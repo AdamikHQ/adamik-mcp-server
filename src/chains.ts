5a9a667bcc25c431a03eb847c8ef2684d675b714
fabrice-adamik <159912605+fabrice-adamik@users.noreply.github.com>
2025-06-13 15:08:35 +0200

    Update chains.ts

--- src/chains.ts
+++ src/chains.ts
@@ -2,6 +2,10 @@
 // Get the full list of supported chains at https://docs.adamik.io/api-reference/chain/get-chain-details
 export default [
   "ethereum",
+  "cosmoshub",
+  "axelar",
+  "dydx",
+  "osmosis",
   "bitcoin",
   "babylon",
   "starknet",
